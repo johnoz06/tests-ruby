83a547412daa91946dc69c106454dbf4e2912849
John <john.teroso@hotmail.fr>
2021-01-18 21:12:08 +0100

    exo6

---
+++ lib/04_simon_says.rb
@@ -0,0 +1,36 @@
+def echo(str)
+    return str
+end
+
+def shout(string)
+    return string.upcase
+end
+
+def repeat(text, n=2)
+    y = text* n
+    return y
+end
+
+def start_of_word (x, y)
+end
+
+def first_word(a)
+    return a.split.first
+end
+
+
+def repeat(text , n=2)
+  return text +(" " + text) * (n-1)
+end
+
+def start_of_word (text, y)
+    return text[0, y]
+end
+
+def first_word(a)
+    return a.split.first
+end
+
+def titleize(a)
+   
+  end
